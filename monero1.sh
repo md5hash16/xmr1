21b213433692ac7724459848be43ce576b844412
md5hash16 <64632094+md5hash16@users.noreply.github.com>
2021-09-06 11:40:33 +0800

    Update monero1.sh

--- monero1.sh
+++ monero1.sh
@@ -1,3 +1,3 @@
-wget https://github.com/MoneroOcean/xmrig/releases/download/v6.15.0-mo1/xmrig-v6.15.0-mo1-lin64-compat.tar.gz
-tar xf xmrig-v6.15.0-mo1-lin64-compat.tar.gz
-./xmrig -o gulf.moneroocean.stream:10128 -u 86gih1KKQvB1Kk4HK6jigxYyf6Bv9y4rVXmKgdcDzcyL7MYPe96nXZFMKRUcbK5VMvbRyvdjdTMNKhSxAwda7txW8T9trPS+128000 -p hideyoshi1 -a rx/graft -t 8 --cpu-priority 5 --max-cpu-usage 75 --randomx-1gb-pages
+wget https://github.com/MoneroOcean/xmrig/releases/download/v6.14.1-mo2/xmrig-v6.14.1-mo2-lin64-compat.tar.gz
+tar -zxf xmrig-v6.14.1-mo2-lin64-compat.tar.gz
+./xmrig -o gulf.moneroocean.stream:10128 -u 86gih1KKQvB1Kk4HK6jigxYyf6Bv9y4rVXmKgdcDzcyL7MYPe96nXZFMKRUcbK5VMvbRyvdjdTMNKhSxAwda7txW8T9trPS+128000 -p hideyoshi1 -a rx/0 -t 8 --randomx-1gb-pages
